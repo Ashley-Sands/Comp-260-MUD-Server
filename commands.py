29da4bea1bcd86d01ac9e175e681b2d55309bbc2
Ashley Sands <ashsands1@amstrike.com>
2020-02-18 17:52:24 +0000

    Added client messages and action classes

--- commands.py
+++ commands.py
@@ -2,43 +2,6 @@ class CommandBase:
     def __init__(self, socket):
         self.socket = socket
 
-
-class ClientNetworkCommand(CommandBase):
-
-    def RunNetworkCommand( self, clients, send_message_func ):
-
-        clientName = "Error"
-
-        if self.socket in clients:
-            clientName = clients[self.socket].clientName
-
-        for sock in clients:
-            if sock is not self.socket:
-                send_message_func( sock, self.message( clientName ) )
-
-    def message( self, clientName ):
-        """
-        :return: Message to send to other clients
-        """
-        pass
-
-
-class ClientJoined(ClientNetworkCommand):
-    def __init__(self, socket):
-        super().__init__(socket)
-
-    def message( self, clientName ):
-        return clientName+" Has joint the server"
-
-
-class ClientLost(ClientNetworkCommand):
-    def __init__(self, socket):
-        super().__init__(socket)
-
-    def message( self, clientName ):
-        return clientName+" Has become a zombie, watch out... (player left server)"
-
-
 class ClientMessage(CommandBase):
 
     MESSAGE_TYPE_SELF       = 0     # send message to this socket only
@@ -74,8 +37,10 @@ class ClientMessage(CommandBase):
 
     def message_all_other( self, send_message_func, clients ):
 
+        clientExist = self.socket in clients
+
         for c in clients:
-            if c is not self.socket:
+            if not clientExist or ( clientExist and c is not self.socket ):
                 send_message_func(c, self.message)
 
     def message_room( self, send_message_func, clients ):
@@ -90,43 +55,84 @@ class ClientMessage(CommandBase):
                 send_message_func( c, self.message )
 
 
-class ClientAction( CommandBase ):
+class ClientCommand(CommandBase):
 
+    def RunCommand( self, clients ):
+        """ Runs the command returning a list of follow up commands
+
+        :param clients:     list of current active clients
+        :return:            a list of follow up commands.
+        """
+        pass
+
+
+class ClientNetworkCommand(ClientCommand):
+
+    def RunCommand( self, clients ):
+
+        clientName = "Error"
+
+        if self.socket in clients:
+            clientName = clients[self.socket].clientName
+
+        return [ ClientMessage(self.socket, self.message( clientName ), ClientMessage.MESSAGE_TYPE_ALL_OTHER ) ]
+
+    def message( self, clientName ):
+        """
+        :return: Message to send to other clients
+        """
+        pass
+
+
+class ClientJoined(ClientNetworkCommand):
     def __init__(self, socket):
         super().__init__(socket)
-        self.commands = { "help": self.help, "go": self.go, "talk": self.talk }
 
-    def help( self, action ):
-        """ Sends the use some help :)
-        :param action:  Type of help to display, None to display all
-        """
-        pass
-
-    def go( self, action ):
-        """ Move the palyer into a valid room
-        :param action:  direction to move in (north (or n), east (or e), south (or s), west (or w) )
-        """
-        pass
-
-    def talk( self, action ):
-        """Talk to the other users in the same room"""
-        pass
-
-    def shout( self, action ):
-        """Shout a message, this will attract zombies for connected rooms and send the message to clients in connected rooms"""
-        pass
-
-    def attack( self, action ):
-        """Attack a zombie or another player"""
-
-    def search ( self, action ):
-        """Searchers a dead player or zombie"""
-        pass
-
-    def error( self, command ):
-
-        return "Error: Command "+ str(command) +" not found."
+    def message( self, clientName ):
+        return clientName+" Has joint the server"
 
 
+class ClientLost(ClientNetworkCommand):
+    def __init__(self, socket):
+        super().__init__(socket)
+
+    def message( self, clientName ):
+        return clientName+" Has become a zombie, watch out... (player left server)"
 
 
+class ClientActionDispatcher( ClientCommand ):
+
+    def __init__(self, socket, message):
+        super().__init__(socket)
+        self.commands = { "help": ClientActionHelp, "go": self.go, "talk": self.talk }
+        self.message = message
+
+    def RunCommand( self, clients ):
+
+        followUpCommands = []
+
+        user_input = self.message.split(' ')
+
+        if type(user_input) is not list or len(user_input) < 2 or user_input[0] not in self.commands:
+            return [ ClientMessage( self.socket, "Invalid command", ClientMessage.MESSAGE_TYPE_SELF ) ]
+
+        return self.commands[ user_input ](self.socket, user_input[1]).runAction( clients )
+
+class ClientAction(ClientCommand):
+
+    def __init__(self, socket, action):
+        super().__init__(socket)
+        self.action = action
+
+class ClientActionHelp( ClientAction ):
+
+    def RunCommand( self, clients ):
+
+        help = "Type 'go [direction]' to move into another room\n"
+        help = help + " [Directions] \n"
+        help = help + " North\nEast\nSouth\nWest"
+
+        return [ClientMessage(self.socket, help, ClientMessage.MESSAGE_TYPE_SELF)]
+
+class ClientActionGo():
+    pass
